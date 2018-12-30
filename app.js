1ca540d711d351181e471a3f78f4e1bafa827663
SebastianM <sebastian.1546@gmail.com>
2018-12-30 15:09:50 -0500

    Terminando header y nav

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("Demn right...");
+navIcon = document.querySelector(".nav-icon"),
+	navMenu = document.querySelector(".nav-menu"),
+	stickyBar = document.querySelector("#sticky");
+keyScrollPrev = window.pageYOffset
+
+
+navIcon.addEventListener("click", function () {
+	navIcon.classList.toggle("open");
+	navMenu.classList.toggle("nav-menu-display");
+});
+
+
+window.addEventListener('scroll', function () {
+	keyScrollCurrent = window.pageYOffset
+	if (keyScrollPrev < keyScrollCurrent || keyScrollPrev == 0) {
+		stickyBar.classList.add("is-sticky")
+	}
+	else {
+		stickyBar.classList.remove("is-sticky")
+	}
+
+	keyScrollPrev = keyScrollCurrent
+
+});
+
+
