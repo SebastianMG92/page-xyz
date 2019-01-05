ea7542da2aa9bc2d9f70fa15327a81d0a569870b
SebastianM <sebastian.1546@gmail.com>
2019-01-05 13:07:37 -0500

    Terminando hero sin formulario

--- app.js
+++ app.js
@@ -1,33 +1,58 @@
-navIcon = document.querySelector(".nav-icon"),
-	navMenu = document.querySelector(".nav-menu"),
-	stickyBar = document.querySelector("#sticky");
+o = {
+	navIcon: document.querySelector(".nav-icon"),
+	navMenu: document.querySelector(".nav-menu"),
+	stickyBar: document.querySelector("#sticky"),
+	inputForms: document.querySelectorAll(".input input"),
+}
 keyScrollPrev = window.pageYOffset
 
+m = {
+	hamburguerMenu: function () {
+		o.navIcon.addEventListener("click", function () {
+			o.navIcon.classList.toggle("open");
+			o.navMenu.classList.toggle("nav-menu-display");
+		})
+	},
+	stickyMenu: function () {
+		window.addEventListener('scroll', function () {
+			keyScrollCurrent = window.pageYOffset
+			if (keyScrollPrev < keyScrollCurrent) {
+				o.stickyBar.classList.remove("is-sticky");
+				o.navIcon.classList.remove("open");
+				o.navMenu.classList.add("nav-menu-display");
+				o.stickyBar.classList.add("hidde");
+			}
+			else if (keyScrollCurrent == 0) {
+				o.stickyBar.classList.remove("is-sticky");
+			}
+			else {
+				o.stickyBar.classList.add("is-sticky");
+				o.stickyBar.classList.remove("hidde");
+			}
 
-navIcon.addEventListener("click", function () {
-	navIcon.classList.toggle("open");
-	navMenu.classList.toggle("nav-menu-display");
-});
+			keyScrollPrev = keyScrollCurrent
+		});
+	},
+	formValidator : function(){
+		for (let i = 0; i < o.inputForms.length; i++) {
+			o.inputForms[i].addEventListener("focusout", function(){
+				console.log(i);
+			if (o.inputForms[i].value != "") {
+				o.inputForms[i].classList.add("filled")
+			}
+			else {
+				o.inputForms[i].classList.remove("filled")
+			}
+			});
+		}
+	},
+}
+
+m.hamburguerMenu();
+m.stickyMenu();
+m.formValidator();
 
 
-window.addEventListener('scroll', function () {
-	keyScrollCurrent = window.pageYOffset
-	if (keyScrollPrev < keyScrollCurrent) {
-		stickyBar.classList.remove("is-sticky");
-		navIcon.classList.remove("open");
-		navMenu.classList.add("nav-menu-display");
-		stickyBar.classList.add("hidde");
-	}
-	else if (keyScrollCurrent == 0) {
-		stickyBar.classList.remove("is-sticky");
-	}
-	else {
-		stickyBar.classList.add("is-sticky");
-		stickyBar.classList.remove("hidde");
-	}
 
-	keyScrollPrev = keyScrollCurrent
-
-});
 
 
