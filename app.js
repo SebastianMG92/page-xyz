8bf0c08dc8b2f6e4fccf6bf5ff6388c755f69d8a
SebastianM <sebastian.1546@gmail.com>
2019-01-15 19:48:43 -0500

    Terminando responsive y empezado about

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
 o = {
-	navIcon: document.querySelector(".nav-icon"),
-	navMenu: document.querySelector(".nav-menu"),
-	stickyBar: document.querySelector("#sticky"),
-	inputForms: document.querySelectorAll(".input input"),
+	navIcon: document.querySelector('.nav-icon'),
+	navMenu: document.querySelector('.nav-menu'),
+	stickyBar: document.querySelector('#sticky'),
+	inputForms: document.querySelectorAll('.input input'),
+	selectForms: document.querySelector('.input select'),
 }
-keyScrollPrev = window.pageYOffset
+
 
 m = {
 	hamburguerMenu: function () {
-		o.navIcon.addEventListener("click", function () {
-			o.navIcon.classList.toggle("open");
-			o.navMenu.classList.toggle("nav-menu-display");
+		o.navIcon.addEventListener('click', function () {
+			o.navIcon.classList.toggle('open');
+			o.navMenu.classList.toggle('nav-menu-display');
 		})
 	},
 	stickyMenu: function () {
+		keyScrollPrev = window.pageYOffset
 		window.addEventListener('scroll', function () {
 			keyScrollCurrent = window.pageYOffset
 			if (keyScrollPrev < keyScrollCurrent) {
-				o.stickyBar.classList.remove("is-sticky");
-				o.navIcon.classList.remove("open");
-				o.navMenu.classList.add("nav-menu-display");
-				o.stickyBar.classList.add("hidde");
+				o.stickyBar.classList.remove('is-sticky');
+				o.navIcon.classList.remove('open');
+				o.navMenu.classList.add('nav-menu-display');
+				o.stickyBar.classList.add('hidde');
 			}
 			else if (keyScrollCurrent == 0) {
-				o.stickyBar.classList.remove("is-sticky");
+				o.stickyBar.classList.remove('is-sticky');
+				o.stickyBar.classList.remove('hidde');
 			}
 			else {
-				o.stickyBar.classList.add("is-sticky");
-				o.stickyBar.classList.remove("hidde");
+				o.stickyBar.classList.add('is-sticky');
+				o.stickyBar.classList.remove('hidde');
 			}
 
 			keyScrollPrev = keyScrollCurrent
@@ -35,16 +38,22 @@ m = {
 	},
 	formValidator : function(){
 		for (let i = 0; i < o.inputForms.length; i++) {
-			o.inputForms[i].addEventListener("focusout", function(){
-				console.log(i);
-			if (o.inputForms[i].value != "") {
-				o.inputForms[i].classList.add("filled")
+			o.inputForms[i].addEventListener('focusout', function(){
+			if (o.inputForms[i].value != '') {
+				o.inputForms[i].classList.add('filled')
 			}
 			else {
-				o.inputForms[i].classList.remove("filled")
+				o.inputForms[i].classList.remove('filled')
 			}
 			});
 		}
+		o.selectForms.addEventListener("change", function(){
+			if (o.selectForms.value == null) {
+				o.selectForms.classList.remove('filled')
+			} else {
+				o.selectForms.classList.add('filled')
+			}
+		});
 	},
 }
 
